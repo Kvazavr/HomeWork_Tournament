66b50c26edc04dbf671e561fcd1049f428534271
Elena Nekrasova <kvazavr@yandex.ru>
2023-03-29 15:25:48 +0400

    Initial commit

---
+++ src/main/java/ru/netology/Game.java
@@ -0,0 +1,12 @@
+package ru.netology;
+
+import java.util.ArrayList;
+
+public class Game {
+    public boolean registered;
+    ArrayList<String> registeredPlayer = new ArrayList<>();
+    if (registered) {
+        registeredPlayer.add(player.name);
+    }
+
+}
---
+++ src/main/java/ru/netology/Player.java
@@ -0,0 +1,8 @@
+package ru.netology;
+
+public class Player {
+    public int id;
+    public String name;
+    public int strength;
+
+}
